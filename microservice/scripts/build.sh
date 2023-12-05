b70ef7a73331b24e60aefd872deecb9b5bf22185
yh342 <yh342@duke.edu>
2023-12-05 16:54:32 -0500

    Remove unnecessary libraries

--- microservice/scripts/build.sh
+++ microservice/scripts/build.sh
@@ -2,8 +2,4 @@
 set -euxo pipefail
 scriptdir=$(cd $(dirname $0) && pwd)
 
-$scriptdir/build-csharp.sh
-$scriptdir/build-java.sh
-$scriptdir/build-go.sh
 $scriptdir/build-python.sh
-$scriptdir/build-typescript.sh
